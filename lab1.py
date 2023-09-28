b4152d3c23661b1f0f8c8077eaf7f195321f5f75
vadim <vadim.stetsko@yandex.ru>
2023-09-28 11:22:11 +0000

    Выполнено 1 задание (выводится 10 изображений, по одному с каждго класса (папки))

--- lab1.py
+++ lab1.py
@@ -8,28 +8,22 @@ import random
 plt.switch_backend('TkAgg')         # to change backend on Tkinter library
 
 
-def random_select_img(numb_of_img):
+def random_select_img():
     root_dir = 'data_lab1/notMNIST_large'
-    all_files = []
+    rand_images = []
     for folder in os.listdir(root_dir):
         inner_folder = os.path.join(root_dir, folder)
         inner_folder_files = os.listdir(inner_folder)
-        for i in range(len(inner_folder_files)):
-            all_files.append(inner_folder_files[i])
-    return random.choices(all_files, k=numb_of_img)
+        rand_images.append(inner_folder + '/' + random.choice(inner_folder_files))
+    return rand_images
 
-
-numb_of_img = 4
-selected_img = [None] * numb_of_img
-
-fig, ax = plt.subplots(1, 4)
+# Task 1 #
+fig, ax = plt.subplots(2, 5)
 fig.suptitle('Input images')
-
-selected_img = random_select_img(numb_of_img)       # choose random images from all files
-
-for i in range(numb_of_img):
-    ax[i].title.set_text(f'Image {i + 1}')
-    ax[i].imshow(selected_img[i])
+selected_img = random_select_img()       # choose random images from all files
+for i in range(len(selected_img)):
+    selected_img[i] = plt.imread(selected_img[i])
+    plt.subplot(2, 5, i + 1)
+    plt.imshow(selected_img[i])
+    plt.title(f'Image {i + 1}')
 plt.show()
-
-# img1 = plt.imread('data_lab1/notMNIST_large/J/a2F6b28udHRm.png')
